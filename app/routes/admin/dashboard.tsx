baf62035f95df5df20d0b4776a843cc0eb7b053f
hanififran <hanif.antoni@escolifesciences.com>
2025-05-09 15:22:45 +0700

    Dashboard

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Header } from "../components";
 
 const dashboard = () => {
+  const user = { name: "Fumus" };
   return (
-    <div>Dashboard Page</div>
-  )
-}
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋`}
+        description="Track activity, trends and popular destination in real time"
+      />
+      Dashboard Page Contents
+    </main>
+  );
+};
 
-export default dashboard
+export default dashboard;
